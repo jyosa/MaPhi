b7f47d71ef4c3cc7cf6420ddd9934d4703223fb9
Juvenal Yosa <42684155+jyosa@users.noreply.github.com>
2018-09-04 19:27:36 -0500

    Add files via upload

---
+++ Maphi/param.py
@@ -0,0 +1,15 @@
+########################### Modify the following parameter dependig of the calculation that you want to perform. ############################################################
+                                                                                           
+                                                                                           
+npro = "8"               #Number of processor for orca calculation, see manual  #(https://sites.google.com/site/orcainputlibrary/setting-up-orca)            
+orcaEXE = '/home/amaranta/orca/orca' #orca exe path   
+mopacEXE = '/opt/mopac/MOPAC2016.exe'                  #MOPAC executable    
+lev_theory = "B3LYP"                              #Level of theory check orca manual   
+basis_set = '6-31G'                         #Basis set cheack orca manual        
+openbabel = '/home/amaranta/anaconda3/bin/babel'        #OpenBabel executable   
+mol_path = "/home/amaranta/Maphi/NMDA"    
+ph = "-p 7.4"          
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
